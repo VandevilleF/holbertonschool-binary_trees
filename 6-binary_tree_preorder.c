369433481104a85815b69a00d3e15810ad559ead
VandevilleF <flo.vandeville@gmail.com>
2024-01-09 13:57:56 +0100

    Correct betty style for task 6, 7 and 8

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
  * Return: nothing
@@ -11,9 +11,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
         return;
-    
+
     func(tree->n);
-    
+
     binary_tree_preorder(tree->left, func);
     binary_tree_preorder(tree->right, func);
 }
