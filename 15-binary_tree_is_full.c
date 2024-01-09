569948398143323a166f82a7a6e87af9c9e60df8
Mottais <mottais.afy@outlook.fr>
2024-01-09 09:16:43 -0800

    Correction valeur de retour quand tree est NULL dans task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	int node_l = 1, node_r = 1;
 
 	if (tree == NULL)
-	return (2);
+	return (1);
 
 	node_l = binary_tree_is_full(tree->left);
 	node_r = binary_tree_is_full(tree->right);
