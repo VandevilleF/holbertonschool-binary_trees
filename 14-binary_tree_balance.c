f5fb68c848f1affa5a1bfc43d0840278a67aa839
VandevilleF <flo.vandeville@gmail.com>
2024-01-09 22:13:01 +0100

    Add #include "9-binary_tree_height.c" for use recursive of binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
