807691fb59265da8f36b2b154ecaec35af10b8c7
VandevilleF <flo.vandeville@gmail.com>
2024-01-09 14:35:22 +0100

    Correct indentation for betty in task 6, 7, 8 and 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/***
+/**
  * binary_tree_height - that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: nothing
@@ -8,17 +8,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int height_left = 0, height_right = 0;
+	int height_left = 0, height_right = 0;
 
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-        return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
-    /*retourne le max(height_left , height_right) + 1*/
-    if (height_left > height_right)
-        return (height_left + 1);
-    else
-        return (height_right + 1);
+	/*retourne le max(height_left , height_right) + 1*/
+	if (height_left > height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
 }
