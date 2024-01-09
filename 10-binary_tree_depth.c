9e6d3b159d817071512e5c6d39f546062002a248
VandevilleF <flo.vandeville@gmail.com>
2024-01-09 15:51:52 +0100

    Change return comment in task 10 and 12

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
- * Return: 0
+ * Return: depth of tree
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
